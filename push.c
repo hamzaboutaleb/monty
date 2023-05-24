c1865bdb3cb779bad5d9065573d7873d7386db91
boutalebhamza <boutaleb.hm@gmail.com>
2023-05-24 09:37:27 -0700

    errors :'(

--- push.c
+++ push.c
@@ -9,12 +9,12 @@ void push_command(stack_t **stack, unsigned int line_number)
 	stack_t *node;
 	int value;
 
-	if (app.arg == NULL && is_number(app.arg) == 0)
+	value = atoi(app.arg);
+	if (app.arg == NULL || is_number(app.arg) == 0 || value == 0)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	value = atoi(app.arg);
 	node = stack_node(value, NULL, NULL);
 
 	if (*stack == NULL)
